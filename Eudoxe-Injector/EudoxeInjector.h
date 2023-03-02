a1bf57d2df9f38658384248fdf1da09f749b8a0e
Sx-Cheats <65744142+Sx-Cheats@users.noreply.github.com>
2023-03-02 22:00:39 +0100

    Update EudoxeInjector.h

--- Eudoxe-Injector/EudoxeInjector.h
+++ Eudoxe-Injector/EudoxeInjector.h
@@ -13,7 +13,7 @@ public:
         setupUi(this);
         BPInject->click([&] 
         {
-                qDebug() << "Options.Arch           = "     << Options.Arch;
+                qDebug() << "Options.Arch           = "     <<  Options.Arch;
                 qDebug() << "Options.AutoInject     = "     <<  Options.AutoInject;
                 qDebug() << "Options.CloakThread    = "     <<  Options.CloakThread;
                 qDebug() << "Options.DllPath        = "     <<  Options.DllPath;
@@ -21,7 +21,7 @@ public:
                 qDebug() << "Options.InjectMethod   = "     <<  Options.InjectMethod;
                 qDebug() << "Options.LoadDllCopy    = "     <<  Options.LoadDllCopy;
                 qDebug() << "Options.Loader         = "     <<  Options.Loader;
-                qDebug() << "Options.Process        = "     << Options.Process;
+                qDebug() << "Options.Process        = "     <<  Options.Process;
                 qDebug() << "Options.RandomFileName = "     <<  Options.RandomFileName;
                 qDebug() << "Options.RemoveFromPEB  = "     <<  Options.RemoveFromPEB;
                 qDebug() << "Options.Timeout        = "     <<  Options.Timeout;
